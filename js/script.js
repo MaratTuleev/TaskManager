288cf9b3bd8669e1ecc9fa1344e1f70da18e7f31
Marat Tuleev <maratozavar@gmail.com>
2020-04-23 21:19:36 +0300

    add creating task on button push with params from inputs

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-function createTask(name, description, date) {
+function createTask() {
+
+    //creating DOM elements
     let divTask = document.createElement('div');
     let divClose = document.createElement('div');
     let divTaskName = document.createElement('div');
@@ -7,12 +9,27 @@ function createTask(name, description, date) {
     let divDate = document.createElement('div');
     let buttonStatus = document.createElement('button');
 
-    divTaskName.innerHTML = name;
-    divDescription.innerHTML = description;
-    divDate.innerHTML = date;
     buttonStatus.innerHTML = 'start';
     divClose.innerHTML = 'x';
 
+    //fill task from user input
+    let taskName = document.querySelector('.input-name');
+    let taskDescription = document.querySelector('.input-description');
+    divTaskName.innerHTML = taskName.value;
+    divDescription.innerHTML = taskDescription.value; //добавить очистку полей после добавления задачи
+    divDate.innerHTML = new Date().toLocaleString("ru", {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        weekday: 'long'
+    });
+
+    //adding classes
     divTask.classList.add('task');
     divClose.classList.add('close');
     divTaskName.classList.add('task-name');
@@ -21,7 +38,7 @@ function createTask(name, description, date) {
     divDate.classList.add('date');
     buttonStatus.classList.add('status');
 
-
+    //add task to task manager
     let task = document.querySelector('.tasks').appendChild(divTask);
     task.appendChild(divClose);
     task.appendChild(divTaskName);
@@ -29,6 +46,9 @@ function createTask(name, description, date) {
     let taskFooter = task.appendChild(divTaskFooter);
     taskFooter.appendChild(divDate);
     taskFooter.appendChild(buttonStatus);
-}
 
-createTask();
+    divClose.onclick = function () {
+        divTask.remove();
+    }
+}
+document.querySelector('.create-task-btn').onclick = createTask;
